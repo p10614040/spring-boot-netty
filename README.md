8b836c1c664f8daf1e6d5bd57c776796820cf1e2
严亮 <695795023@qq.com>
2017-06-20 08:55:49 +0800

    spring-boot-netty
    使用 Netty 实现的 Demo

---
+++ README.md
@@ -0,0 +1,10 @@
+# SpringBoot Netty
+基于 SpringBoot 的 Netty 程序，测试基于 Netty 的客户端、服务端功能。
+
+* JDK：1.8+
+* SpringBoot：1.5.4.RELEASE
+* spring-data-redis：1.5.4.RELEASE
+* Redis cluster：3.2.7 
+
+## 使用方法
+推荐使用 BoundXXXOperations 相关的类，使用方法见 SpringBootRedisApplicationTests.testRedis()。
---
+++ src/main/java/com/iceblock/springboot/netty/SpringBootNettyApplication.java
@@ -0,0 +1,12 @@
+package com.iceblock.springboot.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNettyApplication.class, args);
+	}
+}
